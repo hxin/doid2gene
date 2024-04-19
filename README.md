f04df5ba45fb4dbfc23e8ee8f683e0525a767d06
hxin <hxin87@gmail.com>
2024-04-19 13:52:15 +0100

    refactor.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # doid2gene
-to update hdo to gene annotation
+
+# R Script for Mapping DOID to Entrez Gene IDs
+Overview
+This R script downloads HDO annotation from [alliancegenome](https://www.alliancegenome.org/), 
+maps gene symbol to entrez Gene IDs using the org.Hs.eg.db package, and writes the mapped data to a CSV file(actually tab separated).
+
+require R library
+```R
+library(dplyr)
+library(readr)
+library(org.Hs.eg.db)
+```
+
+example code
+```R
+Rscript update_AGR.R
+```
--- update_AGR.R
+++ update_AGR.R
@@ -2,11 +2,16 @@ library(dplyr)
 library(readr)
 library(org.Hs.eg.db)
 
+agr_annotation <- "https://fms.alliancegenome.org/download/DISEASE-ALLIANCE_HUMAN.tsv.gz"
+
 # Download and read disease data
-download.file("https://fms.alliancegenome.org/download/DISEASE-ALLIANCE_HUMAN.tsv.gz", "disease.tsv.gz")
+message("Downloading disease data from...")
+download.file(agr_annotation, "disease.tsv.gz")
+message("Reading disease data...")
 disease <- read_tsv(gzfile("disease.tsv.gz"), comment = "#")
 
 # Map DOID to entrez using org.Hs.eg.db
+message("Mapping gene symbol to entrez using org.Hs.eg.db...")
 disease$entrez <- mapIds(org.Hs.eg.db, 
                          keys = disease$DBObjectSymbol, 
                          column = "ENTREZID", 
@@ -14,12 +19,14 @@ disease$entrez <- mapIds(org.Hs.eg.db,
                          multiVals = "first")
 
 # Filter, select, arrange, and distinct
+message("Formating output...")
 doid2gene <- disease %>% 
-  filter(!is.na(entrez)) %>% 
-  select(DOID, DOtermName, entrez) %>% 
+  filter(!is.na(entrez)) %>%
+  dplyr::select(DOID, DOtermName, entrez) %>%
   arrange(DOID, entrez) %>%
   distinct()
 
 # Write to file
 write.table(doid2gene, "flatfile_doid2gene_agr.csv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+message("annotation file generated: ", file.path( getwd(), "flatfile_doid2gene_agr.csv"))
 
